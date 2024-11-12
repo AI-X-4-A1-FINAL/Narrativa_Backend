252865e3aa29e1aab861c944f7a774cad16c0959
stjoo0925 <stjoo0925@gmail.com>
2024-11-12 16:46:20 +0900

    first commit

---
+++ src/main/java/com/nova/narrativa/NarrativaApplication.java
@@ -0,0 +1,13 @@
+package com.nova.narrativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarrativaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NarrativaApplication.class, args);
+    }
+
+}
