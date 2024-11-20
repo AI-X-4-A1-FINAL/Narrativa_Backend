94e5849a2e502ac725ff595319511ee735b049c5
sjs <wltkqdl@gmail.com>
2024-11-20 18:45:26 +0900

    feat: 로그인 유저 id 얻어서 쿠키에 저장 및 front에서 id 상태 관리

--- src/main/java/com/nova/narrativa/domain/user/repository/UserRepository.java
+++ src/main/java/com/nova/narrativa/domain/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.nova.narrativa.domain.user.repository;
 
 import com.nova.narrativa.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,4 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // userId, loginType으로 존재 여부 확인
     boolean existsByUserIdAndLoginType(String user_id, User.LoginType loginType);
+
+    // login user의 id 얻기
+    Optional<User> findIdByUserIdAndLoginType(@Param("userId") String userId, @Param("loginType") User.LoginType loginType);
 }
