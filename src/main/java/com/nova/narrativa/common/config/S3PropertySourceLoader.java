79b5ed187e370539773e777001eb88b8855a4280
ppudding3861 <ppuding3861@gmail.com>
2024-11-26 14:46:28 +0900

    fix : 도커파일 변경

--- src/main/java/com/nova/narrativa/common/config/S3PropertySourceLoader.java
+++ src/main/java/com/nova/narrativa/common/config/S3PropertySourceLoader.java
@@ -1,12 +1,9 @@
 package com.nova.narrativa.common.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.StandardEnvironment;
-import org.springframework.core.io.InputStreamResource;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
@@ -16,7 +13,6 @@ import java.util.Properties;
 public class S3PropertySourceLoader implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
 
     private final S3Client s3Client;
-
     private final String bucketName = "narrativa-backend-yml";
 
     public S3PropertySourceLoader(S3Client s3Client) {
@@ -32,9 +28,8 @@ public class S3PropertySourceLoader implements ApplicationListener<ApplicationEn
                 .bucket(bucketName)
                 .key(key)
                 .build())) {
-            YamlPropertiesFactoryBean yamlFactory = new YamlPropertiesFactoryBean();
-            yamlFactory.setResources(new InputStreamResource(inputStream));
-            Properties properties = yamlFactory.getObject();
+            Properties properties = new Properties();
+            properties.load(inputStream);
 
             StandardEnvironment environment = (StandardEnvironment) event.getEnvironment();
             environment.getPropertySources().addFirst(new PropertiesPropertySource("s3Properties", properties));
