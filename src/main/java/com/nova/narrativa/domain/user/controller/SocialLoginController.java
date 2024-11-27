5a7c07d581334977eb62354e7b14f1fdc23fbd0b
sjs <wltkqdl@gmail.com>
2024-11-27 19:24:34 +0900

    fix: id cookie 대신 header로 보내기 test

--- src/main/java/com/nova/narrativa/domain/user/controller/SocialLoginController.java
+++ src/main/java/com/nova/narrativa/domain/user/controller/SocialLoginController.java
@@ -98,11 +98,12 @@ public class SocialLoginController {
             // Session Cookie 생성 (브라우저 닫으면 쿠키 삭제)
             
             // http, https 환경 동작
-            log.info("http, https 환경 cookie test 중입니다.");
-            String idCookie = String.format("id=%d; SameSite=None; Secure; Path=/", dbId);
-
-            log.info("idCookie: {}", idCookie);
-            response.setHeader("Set-Cookie", idCookie);
+//            log.info("http, https 환경 cookie test 중입니다.");
+//            String idCookie = String.format("id=%d; SameSite=None; Secure; Path=/", dbId);
+//
+//            log.info("idCookie: {}", idCookie);
+//            response.setHeader("Set-Cookie", idCookie);
+            response.setHeader("id", "123");
 
             response.setStatus(HttpServletResponse.SC_CREATED);
         } catch (Exception e) {
